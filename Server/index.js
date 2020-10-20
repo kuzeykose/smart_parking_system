069cf81c258fd33195723034c342c5100e2f9657
kuzeykose <33777045+kuzeykose@users.noreply.github.com>
2020-10-20 07:34:26 +0300

    server communication with firebase

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+
+// GET 
+function getDataFirestore() {
+
+  try {
+    //firestore üzerinde dota/heroes pathini referans alıyor
+    const docRef = db.collection("car-parks");
+
+    docRef.doc("BilgiUniversitySantralCarPark").collection("parking-slots").doc("A01").get().then((data) => {
+      if (data && data.exists) {
+        const responseData = data.data();
+        console.log(JSON.stringify(responseData, null, ""));
+      }
+    })
+  } catch (error) { }
+
+}
+
+getDataFirestore();
+
+
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
