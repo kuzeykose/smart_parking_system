cfa953a834bc93d0ea533c3d0f0679a5cde045c6
kuzeykose <33777045+kuzeykose@users.noreply.github.com>
2020-11-17 20:47:36 +0300

    adding routes cars api for getting brands and data for cars picker

---
+++ Server/routes/api/cars.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const { admin } = require('../../firebaseConfig')
+
+//get Brands of car in database
+const db = admin.firestore();
+router.get('/', (req, res) => {
+  const docRef = db.collection("car-models");
+  docRef
+    .get()
+    .then(doc => {
+      const documents = doc.docs.map(document => {
+        return { label: document.id, value: document.id }
+      })
+      res.send(documents)
+    })
+});
+
+router.post('/selected-car', (req, res) => {
+  res.send(req.body.selectedCarModel)
+
+  if (req.body.selectedCarModel === undefined) {
+    console.log("sorry");
+  } else {
+    const docRef = db.collection("car-models").doc(req.body.selectedCarModel)
+    docRef
+      .get()
+      .then(col => {
+
+      })
+  }
+
+});
+
+
+
+module.exports = router
